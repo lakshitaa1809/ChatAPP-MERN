a7cc787b995800060d49c699975c8b6fa7dd3bc1
Saranya <tsaranya03@gmail.com>
2023-07-25 17:13:18 +0530

    add

--- connect/src/components/firebase.js
+++ connect/src/components/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
